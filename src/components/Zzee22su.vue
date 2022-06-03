5c4bd4d487a2e413be8cd0c0fd095db7b320f7b3
zzee22su <patrickstar0910@gmail.com>
2022-06-03 17:17:18 +0900

    ADD : slot 사용하기

--- src/components/Zzee22su.vue
+++ src/components/Zzee22su.vue
@@ -1,14 +1,15 @@
 <template>
     <div>
-        <h1>
-            {{ title }}
-        </h1>
-        <p>
-            {{ name }}
-        </p>
-        <button @click="updateName">
-            Change Name
-        </button>
+        <h1>{{ title }}</h1>
+        <!-- 다른 html 코드를 사용하고 싶을 때, slot을 사용한다. -->
+        <p>{{ name }}</p>
+        <button @click="updateName">Change Name</button>
+
+        <p>Header</p>
+        <slot name="header" :kossie="kossie"></slot>
+        <p>Body</p>
+        <slot></slot>
+        <p>Footer</p>
     </div>
 </template>
 
@@ -30,6 +31,7 @@ export default {
 
     data() {
         return {
+            kossie: 'coder'
             // name: 'Zzee22su'
         }
     },
@@ -43,4 +45,7 @@ export default {
 </script>
 
 <style scoped>
+p {
+    color: blueviolet;
+}
 </style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <Zzee22su/>
+    <Zzee22su>
+       This is slot area
+       <!-- 
+         v-slot:header은 ->  #header로 줄여서 쓸 수 있다.  
+        
+        방법 1
+        <template #header="props">
+          <p>slot name is header {{ props.kossie }}</p>
+        </template> 
+
+        방법 1을 방법 2처럼 사용할 수 있다. 
+
+
+        방법 2
+        <template #header="{ kossie }">
+          <p>slot name is header {{ kossie }}</p>
+        </template>
+       -->
+      <template #header="{ kossie }">
+        <p>slot name is header {{ kossie }}</p>
+      </template>
+      <template v-slot:default>
+        slot name is default 
+      </template>
+    </Zzee22su>
   </div>
 </template>
 
