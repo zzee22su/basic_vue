ae7f4eff1ac205b253af95807032bc62dc73fb9e
zzee22su <patrickstar0910@gmail.com>
2022-06-03 16:23:17 +0900

    FIX : 자식 컴포넌트 ->부모 컴포넌트로 v-model 사용해서 데이터 보내기

--- src/components/InputField.vue
+++ src/components/InputField.vue
@@ -3,32 +3,67 @@
         <label for="">Name</label>
         <input 
             type="text" 
-            :value="name" 
+            :value="value" 
             style="padding: 30px; border: 2px solid green;"
-            @input="updateName" 
+            @input="$emit('input', $event.target.value)" 
             ><br/>
+
+            <!-- 
+                방법 1
+                <input 
+                    type="text" 
+                    :value="value" 
+                    style="padding: 30px; border: 2px solid green;"
+                    @input="updateName" 
+                    ><br/>
+             -->
+
             <!-- 
                 방법 2
                 @input="updateName"가 아닌 아래처럼 input에 바로 넣는 방법도 있다. 
                 @input="$emit('update-name', $event)" 
                 emit을 사용하려면 아래에 선언된 updateName(e)도 주석처리 해줘야 한다. 
+
+                <input 
+                    type="text" 
+                    :value="value" 
+                    style="padding: 30px; border: 2px solid green;"
+                    @input="$emit('update-name', $event)" 
+                    ><br/>
             -->
+
+            <!-- 
+                방법 3
+                HomeView.vue내 v-model로 설정한 name은 자식 뷰인 InputField.vue에 value로 들어온다.
+                그래서 $event.target.value를 해줘야 한다. 
+             -->
+
+
     </div>
 </template>
 
 <script>
 export default {
-    props: {
-        name: {
-            type: String,
-            required: true
-        }
+    // 방법 1,2
+    // props: {
+    //     name: {
+    //         type: String,
+    //         required: true
+    //     }
+    // },
+
+    // 방법 3
+        props: {
+            value: {
+                type: String,
+                required: true
+            }   
     },
-    methods: {
-        updateName(e) {
-            console.log(e.target.value);
-            this.$emit('update-name', e.target.value);
-        }
-    }
+    // methods: {
+    //     updateName(e) {
+    //         console.log(e.target.value);
+    //         this.$emit('update-name', e.target.value);
+    //     }
+    // }
 }
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,16 +4,34 @@
       This is Home page
     </h1>
     <form action="">
-      <InputField :name="name" @update-name="updateName"/>
+      <InputField v-model="name"/>
       <button>Submit</button>
     </form>
     {{ name }}
+
+    <!-- 
+      방법 1
+      <InputField :name="name" @update-name="updateName"/>
+      
+      자식이(InputField) 보낸 값을 부모가(HomeView) 받기 위해서 
+      자식 뷰내에 updateName(e)에서 emit name으로 설정한 'update-name'을 
+      이벤트 이름으로 설정한다 -> @update-name 그리고 아래 method에 updateName도 설정해준다.
+     -->
+
     <!-- 
       방법 2
       InputField.vue에서 보낸 $event를
       @update-name="name = $event.target.value"로 받을 수 있다. 
       아래에 선언된 method updateName(name)은 주석처리 해야 한다. 
+
+      <InputField :name="name" @update-name="name = $event.target.value"/>
      -->
+
+     <!-- 
+       방법 3
+       컴포넌트에도 v-model을 사용할 수 있다. 
+       InputFiled에 v-model을 넣어주면 자동으로 자식 컴포넌트에 value로 보내게 된다. 
+      -->
   </div>
 </template>
 
